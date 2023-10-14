3ae8d1f3ca826c04e930af27637fc926390b9973
Tran Phuoc <thientran01345@icloud.com>
2023-10-14 18:29:19 +0700

    CapNhatLab5

--- app/src/main/java/com/example/lab5/Bai3.java
+++ app/src/main/java/com/example/lab5/Bai3.java
@@ -2,12 +2,20 @@ package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Rect;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -15,12 +23,15 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Random;
+
 public class Bai3 extends AppCompatActivity {
     // Bài3
     FloatingActionButton fab3;
-    ImageView imgViewBird, imgViewCloud;
+    ImageView imgViewBird, imgViewCloud, imgViewCloud2, imgViewBirdKill;
     Button btnJump2;
     ImageButton imgBtnUp2, imgBtnDown2, imgBtnLeft2, imgBtnRight2, imgBtnFast, imgBtnLow;
+    public int screenWidth, screenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +54,12 @@ public class Bai3 extends AppCompatActivity {
         imgBtnLow = (ImageButton) findViewById(R.id.imgBtnLow);
         imgViewBird = (ImageView) findViewById(R.id.imgViewBird);
         imgViewCloud = (ImageView) findViewById(R.id.imgViewCloud);
+        imgViewCloud2 = (ImageView) findViewById(R.id.imgViewCloud2);
+        imgViewBirdKill = (ImageView) findViewById(R.id.imgViewBridKill);
         imgViewBird.setBackgroundResource(R.drawable.animation);
         imgViewCloud.setBackgroundResource(R.drawable.animationclound);
+        imgViewCloud2.setBackgroundResource(R.drawable.animationclound);
+        imgViewBirdKill.setBackgroundResource(R.drawable.animation_bird_kill);
 
     }
 
@@ -113,20 +128,61 @@ public class Bai3 extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+
+        // lấy kích thước màn hình
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenWidth = displayMetrics.widthPixels;
+        screenHeight = displayMetrics.heightPixels;
+
+        startBirdKillAnimation();
+    }
+
+    public void startBirdKillAnimation() {
+        // Bắt đầu animation
+        ObjectAnimator objectAnimatorX = ObjectAnimator.ofFloat(imgViewBirdKill, "translationX", screenWidth, -imgViewBirdKill.getWidth());
+        ObjectAnimator objectAnimatorY = ObjectAnimator.ofFloat(imgViewBirdKill, "translationY", getRandomYPosition());
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(objectAnimatorX, objectAnimatorY);
+        animatorSet.setDuration(6000);
+        animatorSet.setInterpolator(new LinearInterpolator());
+        animatorSet.start();
+
+        // Lặp lại sau 10s
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Bắt đầu lại animation
+                startBirdKillAnimation();
+            }
+        }, 10000);
+    }
+
+    public float getRandomYPosition() {
+        // Lấy một vị trí Y ngẫu nhiên trong khoảng imgViewCloud
+        Random random = new Random();
+        return random.nextFloat() * (imgViewCloud.getHeight() - imgViewBirdKill.getHeight());
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         AnimationDrawable frameCloud = (AnimationDrawable) imgViewCloud.getBackground();
+        AnimationDrawable frameCloud2 = (AnimationDrawable) imgViewCloud2.getBackground();
         AnimationDrawable frameBird = (AnimationDrawable) imgViewBird.getBackground();
+        AnimationDrawable frameBirdKill = (AnimationDrawable) imgViewBirdKill.getBackground();
 
         if (hasFocus) {
             frameCloud.start();
             frameBird.start();
+            frameBirdKill.start();
+            frameCloud2.start();
         } else {
             frameCloud.stop();
+            frameCloud2.stop();
             frameBird.stop();
+            frameBirdKill.stop();
         }
     }
 }
